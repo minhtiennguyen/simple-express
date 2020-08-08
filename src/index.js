e04c7bf29e8baa570bb52e05bfff0c8e0002a4d3
Minh Tien Nguyen <minhtiennguyen.dev@gmail.com>
2020-08-08 19:17:20 +1000

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
